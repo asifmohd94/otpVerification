98b7626c59143229ad10c4753a45d78ce49e5f14
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-09-15 00:23:51 +0530

    Add files via upload

---
+++ src/components/Success.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Success = () => {
+    return (
+        <div className="logo-container">
+            <img src="https://github.com/nilankh/otpverification/blob/master/Artboard%201.png?raw=true"/>
+        <div className="w">
+            <h1>Welcome to AdmitKard</h1>
+        </div>
+        <p>
+            In order to provide you with a custom experience
+        </p>
+        <p>We need to ask you a few questions</p>
+        <button className="ver">Get Started</button>
+        <p>*This will only take 5min.</p>
+        </div>
+    )
+}
+
+export default Success
