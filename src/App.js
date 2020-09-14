c7e18c719aab78092380b9a443fe3fda6cbedbfe
asifmohd94 <59363614+asifmohd94@users.noreply.github.com>
2020-09-15 00:22:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import React from 'react';
 
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import OptScreen from "./components/OptScreen";
+import Sign from "./components/OptScreen";
+import Success from "./components/OptScreen";
+
+function App() {
+  return (
+   <Router>
+
+<div>
+  <Switch>
+
+   <Route exact path="/" component={Sign} />
+   <Route exact path="/verify" component={OptScreen} />
+   <Route exact path="/success" component={Success} />
+
+  </Switch>
+</div>
+
+   </Router>
+  );
+}
+
+export default App;
